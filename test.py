af61bfb119a74357aaae3b462260ff1eac25b4d0
nagiakash7 <nagiakash7@gmail.com>
2022-04-19 11:40:14 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests,json 
+
+api_key= 'f766200f6843771024be40f6357b641e'
+city_name=input("enter the city \t")
+
+url=f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
+
+obj=requests.get(url)
+
+
+
+x= obj.json()
+
+print(x["main"]["temp"])
